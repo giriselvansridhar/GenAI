df226f4bc05d9159493fe22ee4125b3f123205fa
Giriselvan Sridhar <131362593+giriselvansridhar@users.noreply.github.com>
2025-08-26 15:38:49 +0000

    26-08-2025

---
+++ 2. Control Statements/app.py
@@ -0,0 +1,23 @@
+# The python conditional statements
+# if
+'''
+age = int(input("Enter your age"))
+if age < 18:
+    print("you can't vote now")
+elif age == 18:
+    print("apply for the voter ID")
+else:
+    print("you can vote")
+    '''
+
+
+ #for loop
+
+for i in range(1,10):
+    if i==11:
+        break
+    print(i, end="")
+else:
+    print("loop completed successfully")
+
+
